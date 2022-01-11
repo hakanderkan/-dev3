e10faf9a6e24ad0d1f729bdd7be02ecd918cdf60
hakanderkan <90130889+hakanderkan@users.noreply.github.com>
2022-01-11 16:56:30 +0300

    ödev3.sql

---
+++ ödev3.sql
@@ -0,0 +1,11 @@
+ödev 1 -) select * from country 
+where country like 'A%a' ;
+
+ödev 2 -) select * from country 
+where country like '_____%n' ;
+
+ödev 3-) select title from film
+WHERE title ILIKE '%T%%T%%T%%T%'
+
+ödev 4-) select * from film
+where title like 'C%' and length >90 and  rental_rate =2.99 ;
